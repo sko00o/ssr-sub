7e96778799284c70b7e7afafe056f011e5f13f12
明城 <mingcheng@users.noreply.github.com>
2021-03-04 11:19:34 +0800

    增加测试脚本

--- subscriber_test.go
+++ subscriber_test.go
@@ -2,33 +2,46 @@ package subscriber
 
 import (
   "io/ioutil"
+  "os"
   "testing"
 
   "github.com/stretchr/testify/assert"
 )
 
 func TestFromString(t *testing.T) {
-  test, err := ioutil.ReadFile("./test/2.txt")
-  assert.Nil(t, err)
+  const filePath = "./test/1.txt"
 
-  got, err := FromString(string(test))
-  assert.Nil(t, err)
-  assert.NotEmpty(t, got)
+  if _, err := os.Stat(filePath); os.IsExist(err) {
+    test, err := ioutil.ReadFile(filePath)
+    assert.Nil(t, err)
+
+    got, err := FromString(string(test))
+    assert.Nil(t, err)
+    assert.NotEmpty(t, got)
+  }
 }
 
 func TestFromBytes(t *testing.T) {
-  test, err := ioutil.ReadFile("./test/2.txt")
-  assert.Nil(t, err)
+  const filePath = "./test/1.txt"
 
-  got, err := FromBytes(test)
-  assert.Nil(t, err)
-  assert.NotEmpty(t, got)
+  if _, err := os.Stat(filePath); os.IsExist(err) {
+    test, err := ioutil.ReadFile(filePath)
+    assert.Nil(t, err)
+
+    got, err := FromBytes(test)
+    assert.Nil(t, err)
+    assert.NotEmpty(t, got)
+  }
 }
 
 func TestFromFile(t *testing.T) {
-  got, err := FromFile("./test/1.txt")
-  assert.Nil(t, err)
-  assert.NotEmpty(t, got)
+  const filePath = "./test/1.txt"
+
+  if _, err := os.Stat(filePath); os.IsExist(err) {
+    got, err := FromFile(filePath)
+    assert.Nil(t, err)
+    assert.NotEmpty(t, got)
+  }
 }
 
 func TestFromURL(t *testing.T) {
